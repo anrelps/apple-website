3eb9707491d3e7843cf28530b108320eff0933eb
anrelps <132225545+anrelps@users.noreply.github.com>
2025-02-25 01:02:06 -0300

    Update README and App component structure for iPhone 15 website clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
+import * as Sentry from '@sentry/react';
 import React from 'react';
+import Features from './components/Features';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
-import Navbar from './components/Navbar';
-import Model from './components/Model';
-import * as Sentry from '@sentry/react';
-import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+import Model from './components/Model';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return (
-    <main className='bg-black'>
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-      <Features />
-      <HowItWorks />
-      <Footer />
-    </main>
-  );
+    return (
+        <main className='bg-black overflow-y-auto'>
+            <Navbar />
+            <Hero />
+            <Highlights />
+            <Model />
+            <Features />
+            <HowItWorks />
+            <Footer />
+        </main>
+    );
 };
 
 export default Sentry.withProfiler(App);
